994a5cfbeb75d0f7a1a5a11d7544a60b3d0c8993
dain7672 <dain7672@gmail.com>
2018-07-28 18:50:44 +0900

    add message views

--- CBACampProject/CBACampProject/MassageTabViewController.swift
+++ CBACampProject/CBACampProject/MassageTabViewController.swift
@@ -13,6 +13,7 @@ class MassageTabViewController: UIViewController {
     @IBAction func HambergerAction(_ sender: Any) {
         Hamberger.popIn(fromScale: 1.5, duration: 2, delay: 0)
     }
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var url = URL(string:"http://cbapray.sungrak.or.kr/RetreatSite/RetreatAdd")
     
@@ -35,6 +36,56 @@ class MassageTabViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let height = self.view.frame.height / 2
+        let width = self.view.frame.width / 2
+        super.viewDidLoad()
+        scrollView.frame.size.width = scrollView.frame.size.width + 20
+        let scrollcontainerView = UIView(frame: scrollView.frame)
+        scrollView.addSubview(scrollcontainerView)
+        
+        var inypos = 10
+        let inxpos = 20
+        for i in 0...9 {
+            let cellview = UIView()
+            cellview.layer.borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0).cgColor
+            cellview.layer.borderWidth = 1
+            cellview.backgroundColor = UIColor.white
+            
+            //높이: 기본값으로 둘 수 있을 것이다. 나중에 max등으로 비교해서 적용하도록.
+            cellview.frame = CGRect(x: 10, y: i * 90 + 10, width : Int(scrollView.frame.width), height: 80)
+            scrollView.addSubview(cellview)
+            
+            //profile image, rank name label, name label //////////////////////////////
+            let profileimgview = UIImageView()
+            profileimgview.frame = CGRect(x:10, y:10, width: 40, height: 40)
+            profileimgview.contentMode = UIViewContentMode.scaleAspectFill
+            profileimgview.clipsToBounds = true //image set 전에 해주어야 한다.
+            profileimgview.image = UIImage(named: "profile.png")
+            profileimgview.layer.cornerRadius = profileimgview.frame.width / 2
+            cellview.addSubview(profileimgview)
+            
+            //textview///////////////////////////////////////
+            let textview = UITextView()
+            textview.text = "첫 글입니다 ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ"
+            //textview.font = UIFont(name: "NotoSans", size: 16.0)!
+            textview.frame.origin = CGPoint(x:inxpos + 35, y:10)
+            textview.frame.size = CGSize(width: Int(scrollView.frame.width) - inxpos * 2 - 45, height: 30)
+            let contentSize = textview.sizeThatFits(textview.bounds.size)
+            var frame = textview.frame
+            frame.size.height = max(contentSize.height, 30)
+            textview.frame = frame
+            let aspectRatioTextViewConstraint = NSLayoutConstraint(item: textview, attribute: .height, relatedBy: .equal, toItem: textview, attribute: .width, multiplier: textview.bounds.height/textview.bounds.width, constant: 1)
+            textview.addConstraint(aspectRatioTextViewConstraint)
+            textview.isScrollEnabled = false
+            textview.isEditable = false
+            textview.isUserInteractionEnabled = true
+            textview.backgroundColor = UIColor.lightGray
+            cellview.addSubview(textview)
+            
+        }
+        scrollView.contentSize = CGSize(width: scrollView.frame.width, height: 1000)
+        scrollView.isScrollEnabled = true
+        self.view.addSubview(scrollView)
     }
 
     override func didReceiveMemoryWarning() {
